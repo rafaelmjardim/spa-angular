89db2d6fadf012b6d418e99ba7ccf4e71390dde8
rafaelmjardim <rafaelmjardim@hotmail.com>
2022-09-13 11:40:26 -0300

    Table start

--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -1,24 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
-  name: string,
-  position: number,
-  weight: number,
-  symbol: string
-}
+// PeriodicElement Type
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 3, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 4, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 5, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 6, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 7, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 8, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 9, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-  {position: 10, name: 'Hydron', weight: 1.0079, symbol: 'H'},
-];
+// ELEMENT_DATA: type
 
 @Component({
   selector: 'app-blog',
@@ -27,8 +11,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class BlogComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  // displayedColumns typar e colocar string de ids
+
+  //dataSource = nome do arrayList
 
   constructor() { }
 
