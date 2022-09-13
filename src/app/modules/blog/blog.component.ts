8bd38645bcc1e1fdc43937923713f68df1dfc612
rafaelmjardim <rafaelmjardim@hotmail.com>
2022-09-13 14:13:32 -0300

    Update Input custom

--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -1,8 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 // PeriodicElement Type
+type ClientList = {
+  position: number,
+  name: string,
+  age: number,
+  job: string
+}
 
 // ELEMENT_DATA: type
+const CLIENT_DATA: ClientList[] = [
+  {position: 1, name: 'Rafael', age: 24, job: 'Dev' },
+  {position: 2, name: 'Lucas', age: 20, job: 'Analista' },
+  {position: 3, name: 'Ana', age: 34, job: 'Professor(a)' },
+];
 
 @Component({
   selector: 'app-blog',
@@ -12,8 +23,10 @@ import { Component, OnInit } from '@angular/core';
 export class BlogComponent implements OnInit {
 
   // displayedColumns typar e colocar string de ids
+  displayedColumns: string[] = ['position', 'name', 'age', 'job'];
 
   //dataSource = nome do arrayList
+  dataSource = CLIENT_DATA;
 
   constructor() { }
 
